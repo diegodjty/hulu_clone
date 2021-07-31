2a6f2534593195ff6986797cf4518336398a3cb5
Diego Taveras <diegotaveras2@outlook.com>
2021-07-31 17:30:06 -0400

    Header and Nav components

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import HeaderItems from './HeaderItems';
+
+function Header() {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItems title='HOME' Icon={HomeIcon} />
+                <HeaderItems title='TRENDING' Icon={LightningBoltIcon} />
+                <HeaderItems title='VERIFIED' Icon={BadgeCheckIcon} />
+                <HeaderItems title='COLLECTIONS' Icon={CollectionIcon} />
+                <HeaderItems title='SEARCH' Icon={SearchIcon} />
+                <HeaderItems title='ACCOUNT' Icon={UserIcon} />
+            </div>
+            <Image src="https://links.papareact.com/ua6"
+                   alt="logo"
+                   width={200}
+                   height={100}
+                   className="object-contain"
+            >
+
+            </Image>
+        </header>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Header from './../components/Header';
+import Nav from './../components/Nav';
+import Results from './../components/Results';
 
 export default function Home() {
   return (
@@ -6,6 +9,11 @@ export default function Home() {
       <Head>
         <title>Hulo Clone</title>
       </Head>
+
+      <Header />
+      <Nav />
+      <Results />
+
     </div>
   )
 }
